93334b2cf57474b13dae56569cbec41194125a52
Josue Zarzosa <josue.zarzosa@esailors.de>
2014-09-17 16:30:23 +0000

    Change to yeoman generated site.

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name yoApp
+ * @description
+ * # yoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('savingmexico', [
+    'services',
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.bootstrap'
+  ])
+  .config(function($routeProvider) {
+    $routeProvider.
+    when('/:id', {
+      templateUrl: 'views/main.html',
+      controller: 'FrontpageCtrl'
+    }).
+    otherwise({
+      templateUrl: 'views/main.html',
+      controller: 'FrontpageCtrl'
+    });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,86 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoApp
+ */
+angular.module('savingmexico')
+	.controller('FrontpageCtrl', function($scope, $routeParams, $location, $modal, Customized) {
+
+		if ($routeParams.id) {
+			$scope.id = $routeParams.id;
+
+			Customized.query({
+				id: $scope.id
+			}, function(data) {
+				$scope.data = data;
+			}, function(error) {
+				$location.path('/' + defaultId);
+			});
+		} else {
+			$scope.data = {
+				header: {
+					text1: "Six Year High Schools",
+					text2: "Africa's Unholy War"
+				},
+				caption: {
+					text1: "Saving",
+					text2: "Mexico",
+					text3: "How Enrique Peña Nieto's sweeping reforms have changed the narrative in his narco-stained nation"
+				}
+			};
+		}
+
+		$scope.publish = function() {
+			var frontpage = new Customized();
+			frontpage.header = $scope.data.header;
+			frontpage.caption = $scope.data.caption;
+			saved = frontpage.$save(function(f, putResponseHeaders) {
+				$location.path('/' + f.id);
+			});
+		};
+
+		$scope.open = function() {
+			var modalInstance = $modal.open({
+				templateUrl: 'myModalContent.html',
+				controller: ModalInstanceCtrl,
+				size: 'lg',
+				resolve: {
+					data: function() {
+						return $scope.data;
+					}
+				}
+			});
+			modalInstance.result.then(function(data) {
+				$scope.data = data;
+				$scope.publish();
+			}, function() {
+				$log.info('Modal dismissed at: ' + new Date());
+			});
+		};
+	});
+
+/**
+ * @ngdoc function
+ * @name yoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoApp
+ */
+
+function ModalInstanceCtrl($scope, $modalInstance, data) {
+
+	$scope.data = data;
+
+
+	$scope.ok = function() {
+		$modalInstance.close($scope.data);
+	};
+
+	$scope.cancel = function() {
+		$modalInstance.dismiss('cancel');
+	};
+};
